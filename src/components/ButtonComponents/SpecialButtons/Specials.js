af9738e62831841a1f31d8d38d3c103bbab379a1
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-08-13 15:57:38 -0500

    working on geting the numbers to show

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //import any components needed
-
+import SpecialButton from './SpecialButton'
 //Import your array data to from the provided data file
 
 const Specials = () => {
@@ -12,6 +12,10 @@ const Specials = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {specials.map(item => (
+         <SpecialButton specialChar={item}/>
+       ))}
+
     </div>
   );
 };
