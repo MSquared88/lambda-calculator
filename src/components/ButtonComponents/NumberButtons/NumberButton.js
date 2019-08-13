a06fd45bc735a6e48820a9bc6d9c85eff7f9c784
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-08-13 18:48:49 -0500

    finished styling

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -4,7 +4,7 @@ const NumberButton = props => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className = 'btn btn-numbers'>{props.number}</button>
+      <button className = 'btn btn-numbers' id = {props.number === '0' ? 'btn-0' : null }>{props.number}</button>
     </>
   );
 };
